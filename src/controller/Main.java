5a682035c9085128ecfe501dbfdc6e82fd17ded0
enior <psantosdemora@gmail.com>
2025-05-20 12:30:03 +0200

    yo no ejecutar el main
    (hazlo bro)

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -3,7 +3,53 @@ package controller;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		    
+		        String[] memeLyrics = {
+		            "whenImetUinthesuma",
+		            "andIheldyourhandintheheatajune",
+		            "wewaitedforthesumma",
+		            "itfeltliketheperfectdream",
+		            "burninskinfromthesunbeams",
+		            "wewereyoungandwildandfree",
+		            "uandmeinmidjuly",
+		            "icecreamdrippinonmysneaks",
+		            "dancinundertheskyblu",
+		            "sunscreamonmycheek",
+		            "poolsidevibesandbeats",
+		            "lostinbeatsandsunsetfeels",
+		            "smilesalloveru",
+		            "grillitupatthaparkbbqstyle",
+		            "uweremyfavsummathing",
+		            "flipflopsandsandytoes",
+		            "wheniturnedandlookedu",
+		            "everythingwasgolden",
+		            "chillinwitmysunnieson",
+		            "candyshopintheheatscene",
+		            "windowsdownmusicloud",
+		            "chasinlightinourhearts",
+		            "neonsunsetkissintide",
+		            "uandmedoinnothin",
+		            "evrydaywasajunesong",
+		            "wefellinluvinsumma",
+		            "whenyoulaughinechoinside",
+		            "beachwavesinurhair",
+		            "sippinonlemondreamz",
+		            "nobodycouldstopus",
+		            "thiswasourgoldenszn",
+		            "skateridesandbubblegum",
+		            "sundazeandbarefeet",
+		            "midnightswimsandgiggles",
+		            "summawaslitcuzofu"
+		        };
+
+		        for (String line : memeLyrics) {
+		            System.out.println(line);
+		            try {
+		                Thread.sleep(3000); // Espera 3 segundos entre frases
+		            } catch (InterruptedException e) {
+		                System.err.println("Interrupción durante el delay.");
+		            }
+		        }
 
 	}
 
