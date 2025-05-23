feed868d1c451a4cdf1f7fbd2d8a7a174ab08c7b
enior <psantosdemora@gmail.com>
2025-05-23 09:29:43 +0200

    Enemigo

--- src/model/Enemigo.java
+++ src/model/Enemigo.java
@@ -5,5 +5,13 @@ public class Enemigo extends Personaje {
 	boolean isBoss;
 	boolean isSteve;
 	
+	public Enemigo(String nombre, double vidaMax, double vida, double defensa, double fuerza, double velocidad, boolean isRaid, boolean isBoss, boolean isSteve) {
+		this.nombre = nombre;
+		this.vidaMax = vidaMax;
+		this.vida = vida;
+		this.defensa = defensa;
+		this.fuerza = fuerza;
+		this.velocidad = velocidad;
+	}
 	
 }
